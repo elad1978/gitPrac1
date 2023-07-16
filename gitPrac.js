554e1e49d3ae4ce61db663864991d781fd29716e
elad1978 <efrathalev@gmail.com>
2023-07-16 11:29:35 +0300

    commit to firstBranch

--- gitPrac.js
+++ gitPrac.js
@@ -3,4 +3,7 @@ console.log(massage);
 console.log(massage);
 
 let massage2 = "yo-ho i made it";
-console.log(massage);
+console.log(massage2);
+
+let massage3 = "yo-ho i made it again";
+console.log(massage3);
